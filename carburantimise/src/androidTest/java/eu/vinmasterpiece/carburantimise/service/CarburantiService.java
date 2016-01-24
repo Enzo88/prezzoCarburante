cc10f84b7b0da8d13913dadadf4e1cd71db0c07e
Vincenzo Pimpinella <vincenzo@fedora.hp>
2016-01-24 23:43:48 +0100

    API MISE

---
+++ carburantimise/src/androidTest/java/eu/vinmasterpiece/carburantimise/service/CarburantiService.java
@@ -0,0 +1,7 @@
+package eu.vinmasterpiece.carburantimise.service;
+
+/**
+ * Created by vincenzo on 24/01/16.
+ */
+public class CarburantiService {
+}
---
+++ carburantimise/src/main/java/eu/vinmasterpiece/carburantimise/eu/vinmasterpiece/carburantimise/enumerations/Fuels.java
@@ -0,0 +1,7 @@
+package eu.vinmasterpiece.carburantimise.eu.vinmasterpiece.carburantimise.enumerations;
+
+/**
+ * Created by vincenzo on 24/01/16.
+ */
+public enum Fuels {
+}
