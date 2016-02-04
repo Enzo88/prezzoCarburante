66243c8f747df6abf6666d67b2a64877c84e5019
Enzo88 <vincenzo.pimpinella@gmail.com>
2016-02-04 14:12:26 +0100

    testcase

--- carburantimise/src/test/java/eu/vinmasterpiece/carburantimise/ExampleUnitTest.java
+++ carburantimise/src/test/java/eu/vinmasterpiece/carburantimise/ExampleUnitTest.java
@@ -3,13 +3,16 @@ package eu.vinmasterpiece.carburantimise;
 import android.content.Context;
 import android.net.SSLCertificateSocketFactory;
 
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
 
 import static org.junit.Assert.*;
 
@@ -21,12 +24,24 @@ public class ExampleUnitTest {
 
     @Test
     public void addition_isCorrect() throws Exception {
+       /* SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        sslContext.init(null, null, null);
 
-       /* // The connection URL
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient =
+                HttpClients.custom()
+                        .setSSLSocketFactory(socketFactory)
+                        .build();
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+
+
+        // The connection URL
         String url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q={query}";
 
         // Create a new RestTemplate instance
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
 
         // Add the String message converter
         restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
